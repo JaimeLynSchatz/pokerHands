90a1d06d45ca13d787a5e0a695000e45ce5fd291
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-01-01 07:14:17 -0800

    first commit - this time with retsyn, or at least not unneeded VS files

---
+++ PokerHands/PokerHands/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // starting over with this project for perhaps the sixth time
+            // gotta love switching machines without having your Visual Studio-git integration correct!
+        }
+    }
+}
