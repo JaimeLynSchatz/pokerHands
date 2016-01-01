e305e10270eb914f5c7c71381528b7641c3c21d7
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-01-01 07:37:07 -0800

    add Card, PlayerHand classes

---
+++ PokerHands/PokerHands/PlayerHand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHands
+{
+    class PlayerHand
+    {
+        public Card[] Hand { get; set; }
+        public Card HighCard { get; set; }
+        public String BestHand { get; set; }
+    }
+}
