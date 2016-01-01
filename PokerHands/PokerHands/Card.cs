a8dd811da71085b83ef914387f5191faa5005cee
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-01-01 07:34:07 -0800

    create Card, PlayerHand classes

---
+++ PokerHands/PokerHands/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHands
+{
+    class Card
+    {
+        public String Rank { get; set; }
+        public String Suit { get; set; }
+    }
+}
