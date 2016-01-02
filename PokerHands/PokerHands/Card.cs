da9b6459f84a8013a006cc5529c3ac2eb66b383e
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-01-01 22:57:24 -0800

    a new way to evaluate card rank

--- PokerHands/PokerHands/Card.cs
+++ PokerHands/PokerHands/Card.cs
@@ -15,7 +15,51 @@ namespace PokerHands
         */
 
         public string FullCard { get; set; }
-        public int Rank { get; }
+        public int Rank { get; set; }
+
+        public Card(string FullCard)
+        {
+            // do some parameter checking here so your switch is easier?? Fail fast
+
+            this.FullCard = FullCard;
+            int rankValue = (int)Char.GetNumericValue(this.FullCard[0]);
+            // playing with int val = (int)Char.GetNumericValue('8');
+
+            // what if the whole thing is a switch statement with the default being a number rank?
+            switch (FullCard[0])
+            {
+                case 'J':
+                    // Jack
+                    break;
+
+                case 'Q':
+                    // Queen
+                    break;
+
+                case 'K':
+                    // King
+                    break;
+
+                case 'A':
+                    // Ace
+                    break;
+
+                default:
+                    // it's a basic number card
+                    break;
+
+            }
+
+            if (rankValue >= 2 && rankValue <= 10)
+            {
+                // do something awesome
+            }
+            else  // I think I want to nest a switch statement in an if/else statement and that just seems wrong
+            {
+                // do something even awesomer
+            }
+            
+        }
 
 
 
