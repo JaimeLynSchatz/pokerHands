962412f5f9a149eb75d809ca5521cfe5fadbf7f7
JaimeLynSchatz <jaimelynschatz@gmail.com>
2016-01-10 22:15:00 -0800

    moved test card creation to TestClassInitialize
    Still need to refactor CanCountRanksInAHand() -- it's total, complete white box, and only testing one set. There must be a better way to test that method.

--- PokerHands/PokerHandsTests/HandEvaluatorTests.cs
+++ PokerHands/PokerHandsTests/HandEvaluatorTests.cs
@@ -9,108 +9,47 @@ namespace PokerHandsTests
     public class HandEvaluatorTests
     {
 
-        [TestMethod]
-        public void CanSortCardsByRank()
-        {
-            PlayerHand player1 = new PlayerHand();
-            player1.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
-            player1.Cards.Add(new Card(Rank.Eight, Suit.Clubs));
-            player1.Cards.Add(new Card(Rank.Jack, Suit.Hearts));
-            player1.Cards.Add(new Card(Rank.Nine, Suit.Spades));
-            player1.Cards.Add(new Card(Rank.Two, Suit.Clubs));
-
-            HandEvaluator.SortByRank(player1);
-
-            Assert.AreEqual("{2C,8C,9S,JH,AD}", player1.ToString());
-        }
-        [TestMethod]
-
-        public void HighCardTest()
-        {
-            // Arrange
-            PlayerHand player1 = new PlayerHand();
-            player1.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
-            player1.Cards.Add(new Card(Rank.Eight, Suit.Clubs));
-            player1.Cards.Add(new Card(Rank.Jack, Suit.Hearts));
-            player1.Cards.Add(new Card(Rank.Nine, Suit.Spades));
-            player1.Cards.Add(new Card(Rank.Two, Suit.Clubs));
-
-            // Act
-            Card highCard = new Card(Rank.Two, Suit.Clubs);
-            highCard = HandEvaluator.HighCard(player1);
-
-            // Assert
-            Assert.AreEqual((new Card(Rank.Ace, Suit.Diamonds)).Rank, highCard.Rank);
-        }
-
-        [TestMethod]
-        public void CanDetermineIfHandIsNotAFlush()
-        {
-            // Arrange
-            PlayerHand player1 = new PlayerHand();
-            player1.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
-            player1.Cards.Add(new Card(Rank.Eight, Suit.Clubs));
-            player1.Cards.Add(new Card(Rank.Jack, Suit.Hearts));
-            player1.Cards.Add(new Card(Rank.Nine, Suit.Spades));
-            player1.Cards.Add(new Card(Rank.Two, Suit.Clubs));
+        //Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext) { 
             
-            //Act
-
-
-            // Assert
-            Assert.IsFalse(HandEvaluator.Flush(player1));
-        }
-
-        [TestMethod]
-        public void CanDetermineIfHandIsAFlush()
-        {
-            // Arrange
-            PlayerHand player2 = new PlayerHand();
-            player2.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
-            player2.Cards.Add(new Card(Rank.Eight, Suit.Diamonds));
-            player2.Cards.Add(new Card(Rank.Jack, Suit.Diamonds));
-            player2.Cards.Add(new Card(Rank.Nine, Suit.Diamonds));
-            player2.Cards.Add(new Card(Rank.Two, Suit.Diamonds));
-
-            // Act
-
-            // Assert
-            Assert.IsTrue(HandEvaluator.Flush(player2));
-        }
-
-        [TestMethod]
-        public void CanDetermineIfHandIsAStraight()
-        {
-            // Arrange
+            // A basic hand used for tests that don't require a special hand
+            PlayerHand basicHand = new PlayerHand();
+            basicHand.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
+            basicHand.Cards.Add(new Card(Rank.Eight, Suit.Clubs));
+            basicHand.Cards.Add(new Card(Rank.Jack, Suit.Hearts));
+            basicHand.Cards.Add(new Card(Rank.Nine, Suit.Spades));
+            basicHand.Cards.Add(new Card(Rank.Two, Suit.Clubs));
+            string basicHandStringSorted = "{2C,8C,9S,JH,AD}"
+            
+            PlayerHand aceOfDiamondHighCard = new PlayerHand();
+            aceOfDiamondHighCard.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
+            aceOfDiamondHighCard.Cards.Add(new Card(Rank.Eight, Suit.Clubs));
+            aceOfDiamondHighCard.Cards.Add(new Card(Rank.Jack, Suit.Hearts));
+            aceOfDiamondHighCard.Cards.Add(new Card(Rank.Nine, Suit.Spades));
+            aceOfDiamondHighCard.Cards.Add(new Card(Rank.Two, Suit.Clubs));
+            
             PlayerHand straightHand = new PlayerHand();
             straightHand.Cards.Add(new Card(Rank.Three, Suit.Diamonds));
             straightHand.Cards.Add(new Card(Rank.Six, Suit.Diamonds));
             straightHand.Cards.Add(new Card(Rank.Five, Suit.Diamonds));
             straightHand.Cards.Add(new Card(Rank.Four, Suit.Diamonds));
             straightHand.Cards.Add(new Card(Rank.Two, Suit.Diamonds));
-
-            // Assert
-            Assert.IsTrue(HandEvaluator.Straight(straightHand));
-        }
-
-        [TestMethod]
-        public void CanDetermineIfHandIsARoyalFlush()
-        {
+            
+            PlayerHand flushHand = new PlayerHand();
+            flushHand.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
+            flushHand.Cards.Add(new Card(Rank.Eight, Suit.Diamonds));
+            flushHand.Cards.Add(new Card(Rank.Jack, Suit.Diamonds));
+            flushHand.Cards.Add(new Card(Rank.Nine, Suit.Diamonds));
+            flushHand.Cards.Add(new Card(Rank.Two, Suit.Diamonds));
+            
             PlayerHand royalFlushHand = new PlayerHand();
             royalFlushHand.Cards.Add(new Card(Rank.Ace, Suit.Clubs));
             royalFlushHand.Cards.Add(new Card(Rank.Ten, Suit.Clubs));
             royalFlushHand.Cards.Add(new Card(Rank.Queen, Suit.Clubs));
             royalFlushHand.Cards.Add(new Card(Rank.Jack, Suit.Clubs));
             royalFlushHand.Cards.Add(new Card(Rank.King, Suit.Clubs));
-
-            HandEvaluator.SortByRank(royalFlushHand);
-
-            Assert.IsTrue(HandEvaluator.RoyalFlush(royalFlushHand));
-        }
-
-        [TestMethod]
-        public void CanDetermineIfHandIsAStraightFlush()
-        {
+            
             PlayerHand straightFlushHand = new PlayerHand();
             straightFlushHand.Cards.Add(new Card(Rank.Five, Suit.Hearts));
             straightFlushHand.Cards.Add(new Card(Rank.Four, Suit.Hearts));
@@ -118,14 +57,74 @@ namespace PokerHandsTests
             straightFlushHand.Cards.Add(new Card(Rank.Three, Suit.Hearts));
             straightFlushHand.Cards.Add(new Card(Rank.Six, Suit.Hearts));
 
-            HandEvaluator.SortByRank(straightFlushHand);
+            
+        }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        [TestMethod]
+        public void CanSortCardsByRank()
+        {  
+            Assert.AreEqual(basicHandStringSorted, HandEvaluator.SortByRank(player1).ToString());
+        }
+        [TestMethod]
 
+        public void HighCardTest()
+        {
+            Card highCard = new Card(Rank.Two, Suit.Clubs);
+            highCard = HandEvaluator.HighCard(aceOfDiamondHighCard);
+            Assert.AreEqual((new Card(Rank.Ace, Suit.Diamonds)).Rank, highCard.Rank);
+        }
+
+        [TestMethod]
+        public void CanDetermineIfHandIsNotAFlush()
+        {
+            // Assert
+            Assert.IsFalse(HandEvaluator.Flush(basicHand));
+        }
+
+        [TestMethod]
+        public void CanDetermineIfHandIsAFlush()
+        {
+            Assert.IsTrue(HandEvaluator.Flush(flushHand));
+        }
+
+        [TestMethod]
+        public void CanDetermineIfHandIsAStraight()
+        {
+            Assert.IsTrue(HandEvaluator.Straight(straightHand));
+        }
+
+        [TestMethod]
+        public void CanDetermineIfHandIsARoyalFlush()
+        {
+            // test with this sort removed - this really should be in the evaluator, not the test
+            //HandEvaluator.SortByRank(royalFlushHand);
+            Assert.IsTrue(HandEvaluator.RoyalFlush(royalFlushHand));
+        }
+
+        [TestMethod]
+        public void CanDetermineIfHandIsAStraightFlush()
+        {
+            // test with this sort removed - this really should be in the evaluator, not the test
+            //HandEvaluator.SortByRank(straightFlushHand);
             Assert.IsTrue(HandEvaluator.StraightFlush(straightFlushHand));
         }
 
         [TestMethod]
         public void CanCountRanksInAHand()
         {
+            // this one needs some work, leaving here for now -- what's a better way??
             PlayerHand player1 = new PlayerHand();
             player1.Cards.Add(new Card(Rank.Ace, Suit.Diamonds));
             player1.Cards.Add(new Card(Rank.Eight, Suit.Clubs));
